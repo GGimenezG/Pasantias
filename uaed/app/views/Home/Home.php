488ca00b6565593a26db9bce8fbbd0ce20e2aa0c
AnnRiera <AnnRiera@outlook.com>
2018-09-05 16:26:36 -0400

    Actualización de la vista Home
    Esta es la vista lista de Home.

--- uaed/app/views/Home/Home.php
+++ uaed/app/views/Home/Home.php
@@ -95,49 +95,102 @@
                 <div class="col-sm-12">
                     <!-- Authentication card start -->
                     
-                        <form class="md-float-material form-material" name="login" action="<?= FOLDER_PATH . '/home/login' ?>" method="POST">
+                        <form class="md-float-material form-material" id="login" name="login" action="<?= FOLDER_PATH . '/home/login' ?>" method="POST">
                             <div class="text-center">
                                 <img src="<?php echo PATH_IMG?>/logo.png" alt="logo.png">
                             </div>
                             <div class="auth-box card">
-                                <div class="card-block">
+                              <!-- Aquí empieza el card del login-->
+                                <div id="card-1" class="card-block">
                                     <div class="row m-b-20">
                                         <div class="col-md-12">
                                             <h3 id="inicio" class="text-center" name="inicio">Iniciar Sesión</h3>
                                         </div>
                                     </div>
                                     <div id="user" class="form-group form-primary">
-                                        <input type="text" name="u_username" id="u_username" class="form-control" required="on">
+                                        <input type="text" name="u_username" id="u_username" class="form-control" required="">
                                         <span class="form-bar"></span>
-                                        <label id="usuario" class="float-label" for="u_username"> Usuario </label>
+                                        <label class="float-label" for="u_username"> Usuario </label>
                                     </div>
                                     <div id="pass" class="form-group form-primary">
-                                        <input type="password" name="u_password" id="u_password" class="form-control" required="on">
+                                        <input type="password" name="u_password" id="u_password" class="form-control" required="">
                                         <span class="form-bar"></span>
-                                        <label id="contraseña" class="float-label" for="u_password"> Contraseña </label>
+                                        <label class="float-label" for="u_password"> Contraseña </label>
                                     </div>
-
                                     <div id="forgot" class="row m-t-25 text-left">
                                         <div class="col-12">
                                             <div class="forgot-phone ">
                                                 <a href="#" class="text-right f-w-600"> ¿Olvidaste la contraseña? </a>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div>                             
                                     <div class="row m-t-30">
+                                        <div id="alerta"></div>
                                         <div class="col-md-12">
-                                            <button id="btn1" type="submit" class="btn btn-primary btn-block btn-md"> ENTRAR 
-                                            </button>
+                                            <button id="registro" type="button" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20" onclick="onEnviar();"> Entrar </button>
                                         </div>
                                     </div>
-                                    <hr/>
                                     <div class="row">
                                         <div class="col-md-10">
-                                              <p id="noRegistrado">¿No estás registrado? <a href="#" id="clickRegistrar" class="text-right f-w-600" onclick="Mostrar();"> Click aquí </a></p>
+                                              <p id="noRegistrado">¿No estás registrado?<a href="#" class="text-right f-w-600" onclick="Mostrar();"> Click aquí </a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                              </div>
+                            </form>
+                            <!-- Aquí termina el formulario del login-->
+                            <!-- Aquí empieza el formulario del registrar-->
+                            <form class="md-float-material form-material" id="formRegistro" name="formRegistro" action="<?= FOLDER_PATH . '/home/login' ?>" method="POST">
+                              <div class="auth-box card">
+                                <div id="card-2" class="card-block">
+                                    <div class="row m-b-20">
+                                        <div class="col-md-12">
+                                            <h3 class="text-center" name="registrarse">Registrarse</h3>
+                                        </div>
+                                    </div>
+                                    <div id="usuario" class="form-group form-primary">
+                                        <input type="text" name="u_username" id="u_username" class="form-control" required="">
+                                        <span class="form-bar"></span>
+                                        <label class="float-label" for="u_username"> Usuario </label>
+                                    </div>
+                                    <div id="cedula" class="form-group form-primary">
+                                      <input type="text" name="u_cedula" id="u_cedula" class="form-control" required="">
+                                      <span class="form-bar"></span>
+                                      <label class="float-label" for="u_cedula"> Cédula </label>
+                                    </div>
+                                    <div id="correo" class="form-group form-primary">
+                                        <input type="email" id="u_email" name="u_email" class="form-control" required="">
+                                        <span class="form-bar"></span>
+                                        <label class="float-label" for="u_email"> Correo Electrónico </label>
+                                    </div>
+                                    <div id="confirme" class="form-group form-primary">
+                                        <input type="email" id="cemail" name="cemail" class="form-control" required="">
+                                        <span class="form-bar"></span>
+                                        <label class="float-label"> Confirme Correo </label>
+                                    </div>
+                                    <div id="clave" class="form-group form-primary">
+                                        <input type="password" name="u_password" id="u_password" class="form-control" required="">
+                                        <span class="form-bar"></span>
+                                        <label class="float-label" for="u_password"> Contraseña </label>
+                                    </div>
+                                    <div id="confirmp" class="form-group form-primary">
+                                        <input type="password" id="cpass" name="cpass" class="form-control" required="">
+                                        <span class="form-bar"></span>
+                                        <label class="float-label"> Confirme Contraseña </label>
+                                    </div>                        
+                                    <div class="row m-t-30">
+                                        <div id="mensajes" name="mensajes"></div>
+                                        <div class="col-md-12">
+                                            <button id="registro" type="button" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20" onclick="Registrar();"> Registrar </button>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-10">
                                               <a href="#" id="volver" onclick="Regresar();"> Volver </a>
                                         </div>
                                     </div>
                                 </div>
+                                <!-- Aquí termina el card del registrar-->
                             </div>
                         </form>
                         <!-- end of form -->
@@ -153,12 +206,15 @@
       <img src="<?php echo PATH_IMG?>/portada.png" alt="portada.png" class="imagen">
     </div>
 
-  <script type="text/javascript" src="<?php echo PATH_JS?>/login.js"></script>  
+  <script type="text/javascript" src="<?php echo PATH_JS?>/login.js"></script>
+  <script type="text/javascript" src="<?php echo PATH_JS?>/validaciones.js"></script>
+
   <!-- Required Jquery -->
-  <script type="text/javascript" src="<?php echo PATH_JS?>/jquery.min.js"></script>     
+  <script type="text/javascript" src="<?php echo PATH_JS?>/jquery.min.js"></script>    
   <script type="text/javascript" src="<?php echo PATH_JS?>/jquery-ui.min.js "></script>     
   <script type="text/javascript" src="<?php echo PATH_JS?>/popper.min.js"></script>     
   <script type="text/javascript" src="<?php echo PATH_JS?>/bootstrap.min.js "></script>
+  <script type="text/javascript" src="<?php echo PATH_JS?>/bootstrap.4.0.0.min.js "></script>
   <!-- waves js -->
   <script src="<?php echo PATH_JS?>/waves.min.js"></script>
   <!-- jquery slimscroll js -->
@@ -167,10 +223,10 @@
   <script type="text/javascript" src="<?php echo PATH_JS?>/SmoothScroll.js"></script>     
   <script src="<?php echo PATH_JS?>/jquery.mCustomScrollbar.concat.min.js "></script>
   <!-- i18next.min.js -->
-  <script type="text/javascript" src="bower_components/i18next/js/i18next.min.js"></script>
+  <!--<script type="text/javascript" src="bower_components/i18next/js/i18next.min.js"></script>
   <script type="text/javascript" src="bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
   <script type="text/javascript" src="bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
-  <script type="text/javascript" src="bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
+  <script type="text/javascript" src="bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>-->
   <script type="text/javascript" src="<?php echo PATH_JS?>/common-pages.js"></script>
 
   <script type="text/javascript" class="init">
